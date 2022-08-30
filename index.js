f36879894f5baf795c162ae35a9f99addd442f9e
Gabe Vee <0gabevee0@gmail.com>
2022-08-30 00:13:27 -0600

    refactored dynamic prompt calling and added classes and test files

--- index.js
+++ index.js
@@ -1,117 +1,123 @@
+//dependencies
 const inquirer = require("inquirer");
 const fs = require("fs");
-const Rx = require("rxjs");
+//Classes
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+//variables
+var managerCounter = 0;
+let Team = [];
 
-const manager = [
-  {
-    type: "input",
-    message: "Enter manager's Name:",
-    name: "managerName",
-  },
-  {
-    type: "input",
-    message: "Enter manager's ID:",
-    name: "managerID",
-  },
-  {
-    type: "input",
-    message: "Enter manager's Email:",
-    name: "managerEmail",
-  },
-  {
-    type: "input",
-    message: "Enter manager's Office Number:",
-    name: "managerOffice",
-  },
-  //menu
-  {
-    type: "list",
-    message: "Do you wish to add team members or finish the team?",
-    choices: [
-      "Intern",
-      "Engineer",
-      new inquirer.Separator(),
-      "Finished Making Team",
-    ],
-    name: "teamMake",
-  },
-];
-const engineer = [
-  {
-    type: "input",
-    message: "Enter Engineer's Name:",
-    name: "engineerName",
-  },
-  {
-    type: "input",
-    message: "Enter Engineers's ID:",
-    name: "engineerID",
-  },
-  {
-    type: "input",
-    message: "Enter Engineer's Email:",
-    name: "engineerEmail",
-  },
-  {
-    type: "input",
-    message: "Enter Engineer's GitHub Username:",
-    name: "engUserName",
-  },
-];
-const intern = [
-  {
-    type: "input",
-    message: "Enter Intern's Name:",
-    name: "internName",
-  },
-  {
-    type: "input",
-    message: "Enter Intern's ID:",
-    name: "internID",
-  },
-  {
-    type: "input",
-    message: "Enter Intern's Email:",
-    name: "internEmail",
-  },
-  {
-    type: "input",
-    message: "Enter Intern's School:",
-    name: "internSchool",
-  },
-];
-const menu = [
-  {
-    type: "list",
-    message: "Do you wish to add team members or finish the team?",
-    choices: ["Intern", "Engineer", "Finished Making Team"],
-    name: "teamMake",
-  },
-];
+//Prompt Questions
+const teamMembers = {
+  Manager: [
+    {
+      type: "input",
+      message: "Enter manager's Name:",
+      name: "managerName",
+    },
+    {
+      type: "input",
+      message: "Enter manager's ID:",
+      name: "managerID",
+    },
+    {
+      type: "input",
+      message: "Enter manager's Email:",
+      name: "managerEmail",
+    },
+    {
+      type: "input",
+      message: "Enter manager's Office Number:",
+      name: "managerOffice",
+    },
+  ],
 
-// switch (manager[4].choices) {
-//   case "Engineer":
-//     addEng = manager.concat(engineer);
-//     break;
-// }
-const prompts = new Rx.Subject();
-console.log(prompts);
+  Engineer: [
+    {
+      type: "input",
+      message: "Enter Engineer's Name:",
+      name: "engineerName",
+    },
+    {
+      type: "input",
+      message: "Enter Engineers's ID:",
+      name: "engineerID",
+    },
+    {
+      type: "input",
+      message: "Enter Engineer's Email:",
+      name: "engineerEmail",
+    },
+    {
+      type: "input",
+      message: "Enter Engineer's GitHub Username:",
+      name: "engUserName",
+    },
+  ],
 
-function writeToFile() {
-  inquirer
-    .prompt(manager)
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      if (error.isTtyError) {
-        console.log("Failed to Render Info");
-      }
-    });
+  Intern: [
+    {
+      type: "input",
+      message: "Enter Intern's Name:",
+      name: "internName",
+    },
+    {
+      type: "input",
+      message: "Enter Intern's ID:",
+      name: "internID",
+    },
+    {
+      type: "input",
+      message: "Enter Intern's Email:",
+      name: "internEmail",
+    },
+    {
+      type: "input",
+      message: "Enter Intern's School:",
+      name: "internSchool",
+    },
+  ],
+};
+const addTeam = {
+  type: "list",
+  message: "Choose Employee type:",
+  choices: ["Manager", "Intern", "Engineer"],
+  name: "employeeMake",
+};
+
+function start() {
+  inquirer.prompt(menu).then((answer) => {
+    if (answer.teamMake == "Yes") {
+      addRole();
+    } else {
+      console.log(answer);
+    }
+  });
+}
+
+const menu = {
+  type: "list",
+  message: "Do you want a new Employee?",
+  choices: ["Yes", "No"],
+  name: "teamMake",
+};
+
+function addRole() {
+  inquirer.prompt(addTeam).then((answer) => {
+    if (answer.employeeMake === "Manager" && managerCounter < 1) {
+      managerCounter++;
+      inquirer.prompt(teamMembers.Manager).then((results) => {}); //TODO: finish this code
+    } else {
+      console.log(answer);
+    }
+  });
 }
 
 function init() {
-  //   writeToFile();
+  start();
 }
 
 // Function call to initialize app
